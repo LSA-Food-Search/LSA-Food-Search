834e26909047d178f3b821c54369008b0c2d38a4
saidabary <saidabary05@gmail.com>
2023-03-27 09:53:42 -0400

    updated

---
+++ index.js
@@ -0,0 +1,8 @@
+/* <button id="myButton">Click me!</button> */
+
+
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+  // Code to execute when button is clicked
+});
