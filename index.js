f2ae38a7e96c313552940b8aa79d9956fe79d46c
Aykhan123 <ayaizkhan59@gmail.com>
2023-03-27 13:38:39 -0400

    more progress

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 document.getElementById('foodSearchButton').addEventListener('click', foodSearch )
+let image = document.getElementById('cardimg')
+let name = document.getElementById('card-title')
+let howTo = document.getElementById('instructions')
 
-let card = 
 
 function foodSearch(){
     let text = document.getElementById('userChoice').value.split(" ").join("-")
     fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d88c1672269546ba8bb8c72a0ff5ee7f&query=${text}&maxFat=25&number=5`)
     .then(response => response.json())
-    .then(data => console.log(data))
-    // .then(data => document.getElementById('card-title').innerText = data.results.tittle)
+    .then(data => {
+        name.innerText = data.results[0].title
+        image.src = data.results[0].image
+        
+        
+
+
+
+    })
+    
 
 }
 
