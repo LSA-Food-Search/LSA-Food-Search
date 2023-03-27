046e45ac1fa2c35e83125afb01cf5811a3267be9
Aykhan123 <ayaizkhan59@gmail.com>
2023-03-27 13:45:10 -0400

    pushed up code

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+document.getElementById('foodSearchButton').addEventListener('click', foodSearch )
+let image = document.getElementById('cardimg')
+let name = document.getElementById('card-title')
+let howTo = document.getElementById('instructions')
+
+
+function foodSearch(){
+    let text = document.getElementById('userChoice').value.split(" ").join("-")
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d88c1672269546ba8bb8c72a0ff5ee7f&query=${text}&maxFat=25&number=5`)
+    .then(response => response.json())
+    .then(data => {
+        name.innerText = data.results[0].title
+        image.src = data.results[0].image
+    })
+}
+
+
+
+
+
 let submit = document.getElementById('ingrediantbutton');
 submit.addEventListener('click', consolelog)
 
