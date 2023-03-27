a685d095683c6acf0f55bb8ef5b56079ab15081e
saidabary <saidabary05@gmail.com>
2023-03-27 10:51:08 -0400

    working on the instrcutions for button

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-/* <button id="myButton">Click me!</button> */
 
-
-const myButton = document.getElementById("myButton");
 
 myButton.addEventListener("click", function() {
   // Code to execute when button is clicked
 });
+
+/*
+function fetchInstructions (){
+  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${token}`)
+  .then(response => response.json())
+  .then(data => displayInstructions(data.results))
+  }
+
+  function displayTopStories(stories){
+  console.log(stories);
+
+  }
+}
+
+document.querySelector("#search-form").addEventListener("submit", (e) => {
+  e.preventDefault()
+  const searchTerm = e.target[0].value
+  console.log(`The Search Stories form has been submitted with search term: "${searchTerm}"`)
+  fetchSearchInfo(searchTerm)
+})
+
+
+
+
+
+
+
+
+*/
