f7348ba0ef4cc961fbb017e3e9a11cf3c297d282
saidabary <saidabary05@gmail.com>
2023-03-27 12:09:29 -0400

    still working button

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 
 
-myButton.addEventListener("click", function() {
+.addEventListener("click", function() {
   // Code to execute when button is clicked
+  // 
 });
 
 /*
 function fetchInstructions (){
-  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${token}`)
+  fetch(``)
   .then(response => response.json())
   .then(data => displayInstructions(data.results))
   }
 
-  function displayTopStories(stories){
+  function displayInstructions(stories){
   console.log(stories);
 
   }
 }
 
-document.querySelector("#search-form").addEventListener("submit", (e) => {
+document.querySelector("#").addEventListener("submit", (e) => {
   e.preventDefault()
   const searchTerm = e.target[0].value
   console.log(`The Search Stories form has been submitted with search term: "${searchTerm}"`)
