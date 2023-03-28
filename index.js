65873b0d55c6c027f5d4d5b4829d3ca78576c8a3
saidabary <saidabary05@gmail.com>
2023-03-28 10:59:21 -0400

    using updated version of modal

--- index.js
+++ index.js
@@ -2,27 +2,31 @@
 let instructionBtn= document.getElementById("instructionBtn")
 instructionBtn.addEventListener("click", popModal);
 
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
-function popModal(){
-    
-}
-/*
-
-function fetchInstructions (){
-  fetch(`https://api.spoonacular.com/recipes/analyzeInstructions?key=$ca45345d004549cab8f1b35c3ebbf68a`)
-  .then(response => response.json())
-  .then(data => displayInstructions(data.results))
-  }
-
-  function displayInstructions(steps){
-
-  }
-}
-}
-  
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
 })
 
 
 
+// // let modalButton= document.getElementById("modal-Button");
+// // modal-button.addEventListener("click",() =>{
 
-*/
+// });
+
+// function popModal(){
+    
+// }
+
+// function fetchInstructions (){
+//   fetch(`https://api.spoonacular.com/recipes/analyzeInstructions?key=$ca45345d004549cab8f1b35c3ebbf68a`)
+//   .then(response => response.json())
+//   .then(data => displayInstructions(data.results))
+//   }
+
+  // function displayInstructions(steps){
+  //   // let h5 = document.getElementById(`h5`);
+
+  // }
