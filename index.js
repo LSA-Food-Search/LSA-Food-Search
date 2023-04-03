7b9930678eeacfd75969d544a259c11e26a3f271
EC2 Default User <ec2-user@ip-172-31-66-159.ec2.internal>
2023-04-03 00:03:18 +0000

    added home page

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function foodSearch(e){
 	let cardsection = document.getElementById('cardsection')
 	cardsection.innerHTML = ''
 	if(target.length > 0){
-		console.log(target)
 		document.getElementById('card-container').innerHTML = ''
 	}
 	let text = document.getElementById('userChoice').value.split(" ").join(",+")
@@ -31,14 +30,11 @@ function foodSearch(e){
 			text.style.fontFamily = 'Aclonica';
 			button.id = data[i].id
 			button.id += ' spoon'
-			console.log(button.id)
 		}
 	})
 	}
 	
 }
-//  // 06f48698c5ec48b6ab0083ab6526e794 ---> luis api key for spoontaciular
-// //  d88c1672269546ba8bb8c72a0ff5ee7f ---> ayaiz api key for spoontaciular
 
 
 let submitt = document.getElementById('ingrediantbutton');
@@ -51,18 +47,12 @@ function addcontent(e) {
 	let cardsection = document.getElementById('card-container')
 	cardsection.innerHTML = ''
 	if(target.length > 0){
-		console.log(target)
 		document.getElementById('cardsection').innerHTML = ''
 	}
 	addcardss(length)
-	// let ingrediant_input = document.getElementById('ingrediantinput').value.split(',').join('%2C%20')
-	// if (ingrediant_input.length ===0 ){
-		// document.getElementById('cardsection').innerHTML = ''
-	// }
 	fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes`, optionss)
 	.then(response => response.json())
 	.then(data => {
-		console.log(data)
 		for(let i=0; i<length;i++){
 			let button = document.getElementById(`holder${i}`)
 			let card = document.getElementById(`cardimg${i}`)
@@ -73,10 +63,7 @@ function addcontent(e) {
 			button.id += ' tasty'
 		}
 	})
-	
-	
-	
-	// .catch(err => console.error(err));
+.catch(err => console.error(err));
 }
 
 function addcardss(number) {
@@ -123,91 +110,6 @@ function addcardss(number) {
 	}
 	cardsection.appendChild(row);
 }
-
-// function addcardss(number) {
-// 	for (let i = 0; i < number; i++) {
-// 		let cardsection = document.getElementById('cardsection')
-// 		let div = document.createElement('div');
-// 		div.className = `card`
-// 		div.style.marginTop = '5%';
-// 		div.style.marginBottom = '5%';
-// 		// div.style.property = 'width: 18rem;'
-// 		let img = document.createElement('img')
-// 		img.id = `cardimg${i}`
-// 		img.src = '...'
-// 		img.style.objectFit = 'cover';
-// 		img.style.maxHeight = '200px';
-// 		img.style.width = '100%';
-// 		img.className = 'card-img-top'
-// 		img.alt = "..."
-// 		let divbody = document.createElement('div')
-// 		divbody.className = 'card-body'
-// 		let title = document.createElement('h5')
-// 		let br = document.createElement('br')
-// 		title.id = `card-title${i}`
-// 		title.innerHTML = 'placeHolder'
-// 		title.className = 'card-title'
-// 		let button = document.createElement('a')
-// 		button.id += `holder${i}`
-// 		button.href = '#'
-// 		button.innerHTML = 'See How To Make'
-// 		button.setAttribute('data-bs-toggle', 'modal')
-// 		button.setAttribute('data-bs-target','#exampleModal')
-// 		button.className = 'btn btn-primary'
-// 		divbody.appendChild(title)
-// 		divbody.appendChild(br)
-// 		divbody.appendChild(button)
-// 		div.appendChild(img)
-// 		div.appendChild(divbody)
-// 		cardsection.appendChild(div)
-// 	}
-// }
-
-
-// function addcards(number) {
-// 	let cardsection = document.getElementById('card-container')
-// 	for (let i = 1; i < number; i++) {
-// 		let div = document.createElement('div');
-// 		div.className = `card`
-// 		div.style.marginTop = '5%';
-// 		div.style.marginBottom = '5%';
-// 		// div.style.property = 'width: 18rem;'
-// 		let img = document.createElement('img')
-// 		img.id = `scardimg${i}`
-// 		img.src = '...'
-// 		img.style.objectFit = 'cover';
-// 		img.style.maxHeight = '200px';
-// 		img.style.width = '100%';
-// 		img.className = 'card-img-top'
-// 		img.alt = "..."
-// 		let divbody = document.createElement('div')
-// 		divbody.className = 'card-body'
-// 		let title = document.createElement('h5')
-// 		let br = document.createElement('br')
-// 		title.id = `scard-title${i}`
-// 		title.innerHTML = 'placeHolder'
-// 		title.className = 'card-title'
-// 		let button = document.createElement('a')
-// 		button.id += `sholder${i}`
-// 		button.href = '#'
-// 		button.innerHTML = 'See How To Make'
-// 		button.setAttribute('data-bs-toggle', 'modal')
-// 		button.setAttribute('data-bs-target','#exampleModal')
-// 		button.className = 'btn btn-primary'
-// 		divbody.appendChild(title)
-// 		divbody.appendChild(br)
-// 		divbody.appendChild(button)
-// 		div.appendChild(img)
-// 		div.appendChild(divbody)
-// 		// let h4 = document.createElement('h4')
-// 		// h4.innerHTML = `${i}.`
-// 		// cardsection.appendChild(h4)
-// 		cardsection.appendChild(div)
-// 	}
-// 	// let h4 = document.createElement('h4')
-// 	// h4.innerHTML = 'Dish Results'
-// 	// cardsection.appendChild(h4)
-// }
 
 function addcards(number) {
     let cardsection = document.getElementById('card-container');
@@ -261,7 +163,7 @@ const optionss = {
 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 	}
 };
-//https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&
+
 let cardTarget = document.getElementById('cardsection');
 cardTarget.addEventListener('click', helperFunction);
 
@@ -276,11 +178,9 @@ function helperFunction(e){
     content.innerHTML = ''
   }
 let cardId = e.target.id.split(' ')
-console.log(cardId)
 fetch(`https://tasty.p.rapidapi.com/recipes/get-more-info?id=${cardId[0]}`,optionss)
 .then(res => res.json())
 .then(data => {
-  console.log(data)
   let img = document.getElementById('modalimg');
   img.src = data.thumbnail_url;
   img.style.objectFit = 'cover';
@@ -315,7 +215,6 @@ fetch(`https://api.spoonacular.com/recipes/${cardId[0]}/information?apiKey=06f48
 		let content = document.getElementById("ingul")
     content.innerHTML = ''
 	}
-  console.log(data)
   let img = document.getElementById('modalimg');
   img.src = data.image
   img.style.objectFit = 'cover';
